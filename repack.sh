f116ddce65d17d90076be4a36110b180aa7805df
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-05-05 19:03:02 +0300

    init

---
+++ repack.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+COMMON_VERSION="0.5.2"
+
+epm tool eget https://raw.githubusercontent.com/alt-autorepacked/common/v$COMMON_VERSION/common.sh
+. ./common.sh
+rm ./common.sh
+# . ../common/common.sh
+
+_package="singularityapp"
+
+epm play singularityapp --save-only
+_add_repo_suffix
+download_version=$(_check_version_from_download)
+remote_version=$(_check_version_from_remote)
+
+if [ "$remote_version" != "$download_version" ]; then
+    TAG="v$download_version"
+    _create_release
+    echo "Release created: $TAG"
+else
+    echo "No new version to release. Current version: $download_version"
+fi
+
+rm common.sh
